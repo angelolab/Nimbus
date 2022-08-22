0b122e54c1d4ffb7c4046b49eccda5de0ddc458e
Lenz <lorenz.rumberger@gmail.com>
2022-08-22 16:58:24 -0700

    Added better tests

--- cell_classification/augmentation_pipeline_test.py
+++ cell_classification/augmentation_pipeline_test.py
@@ -7,7 +7,7 @@ import imgaug.augmenters as iaa
 def get_params():
     return {
         # flip
-        "flip_prob": 0.5,
+        "flip_prob": 1.0,
         # affine
         "affine_prob": 0.5, "scale_min": 0.5,
         "scale_max": 1.5, "shear_angle": 10,
@@ -16,9 +16,11 @@ def get_params():
         # rotate
         "rotate_count": [0, 3],
         # gaussian noise
-        "gaussian_noise_prob": 0.5, "gaussian_noise_min": 0.1, "gaussian_noise_max": 0.5,
+        "gaussian_noise_prob": 0.5, "gaussian_noise_min": 0.1,
+        "gaussian_noise_max": 0.5,
         # gaussian blur
-        "gaussian_blur_prob": 0.5, "gaussian_blur_min": 0.1, "gaussian_blur_max": 0.5,
+        "gaussian_blur_prob": 0.5, "gaussian_blur_min": 0.1,
+        "gaussian_blur_max": 0.5,
         # contrast aug
         "contrast_prob": 0.5, "contrast_min": 0.1, "contrast_max": 2.0,
     }
@@ -33,8 +35,12 @@ def test_get_augmentation_pipeline():
 def test_augment_images():
     params = get_params()
     augmentation_pipeline = get_augmentation_pipeline(params)
-    images = np.random.rand(10, 100, 100, 1).astype(np.float32)
-    masks = np.random.randint(0, 2, [10, 100, 100], dtype=np.int32)
+    images = np.zeros([3, 100, 100, 2], dtype=np.float32)
+    masks = np.zeros([3, 100, 100], dtype=np.int32)
+    images[0, :50, :50, :] = 10.1
+    images[1, 50:, 50:, :] = 201.12
+    masks[0, :50, :50] = 1
+    masks[1, 50:, 50:] = 2
     augmented_images, augmented_masks = augment_images(images, masks, augmentation_pipeline)
 
     # check if right types and shapes are returned
@@ -45,5 +51,11 @@ def test_augment_images():
     assert augmented_images.shape == images.shape
     assert augmented_masks.shape == masks.shape
 
-    # check if binary masks are returned
-    assert list(np.unique(augmented_masks)) == [0, 1]
+    # check if images are augmented
+    assert not np.array_equal(augmented_images, images)
+    assert not np.array_equal(augmented_masks, masks)
+
+    # check if images and masks where augmented with the same spatial augmentations approx.
+    assert np.abs(augmented_images[augmented_masks == 0].mean() - images[masks == 0].mean()) < 1
+    assert np.abs(augmented_images[augmented_masks == 1].mean() - images[masks == 1].mean()) < 5
+    assert np.abs(augmented_images[augmented_masks == 2].mean() - images[masks == 2].mean()) < 20
