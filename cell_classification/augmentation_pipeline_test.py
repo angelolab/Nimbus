9ac453b5f4e51be47738cb6e7086bfa493bbf8f4
Lenz <lorenz.rumberger@gmail.com>
2022-08-23 09:43:02 -0700

    Changed tests for the augmentation pipeline

--- cell_classification/augmentation_pipeline_test.py
+++ cell_classification/augmentation_pipeline_test.py
@@ -9,20 +9,20 @@ def get_params():
         # flip
         "flip_prob": 1.0,
         # affine
-        "affine_prob": 0.5, "scale_min": 0.5,
+        "affine_prob": 1.0, "scale_min": 0.5,
         "scale_max": 1.5, "shear_angle": 10,
         # elastic
-        "elastic_prob": 0.5, "elastic_alpha": [0, 5.0], "elastic_sigma": 0.5,
+        "elastic_prob": 1.0, "elastic_alpha": [0, 5.0], "elastic_sigma": 0.5,
         # rotate
         "rotate_count": [0, 3],
         # gaussian noise
-        "gaussian_noise_prob": 0.5, "gaussian_noise_min": 0.1,
+        "gaussian_noise_prob": 1.0, "gaussian_noise_min": 0.1,
         "gaussian_noise_max": 0.5,
         # gaussian blur
-        "gaussian_blur_prob": 0.5, "gaussian_blur_min": 0.1,
+        "gaussian_blur_prob": 0.0, "gaussian_blur_min": 0.1,
         "gaussian_blur_max": 0.5,
         # contrast aug
-        "contrast_prob": 0.5, "contrast_min": 0.1, "contrast_max": 2.0,
+        "contrast_prob": 0.0, "contrast_min": 0.1, "contrast_max": 2.0,
     }
 
 
@@ -55,7 +55,10 @@ def test_augment_images():
     assert not np.array_equal(augmented_images, images)
     assert not np.array_equal(augmented_masks, masks)
 
+    # check if masks are still binary with the right labels
+    assert list(np.unique(augmented_masks)) == [0, 1, 2]
+
     # check if images and masks where augmented with the same spatial augmentations approx.
     assert np.abs(augmented_images[augmented_masks == 0].mean() - images[masks == 0].mean()) < 1
-    assert np.abs(augmented_images[augmented_masks == 1].mean() - images[masks == 1].mean()) < 5
-    assert np.abs(augmented_images[augmented_masks == 2].mean() - images[masks == 2].mean()) < 20
+    assert np.abs(augmented_images[augmented_masks == 1].mean() - images[masks == 1].mean()) < 1
+    assert np.abs(augmented_images[augmented_masks == 2].mean() - images[masks == 2].mean()) < 5
