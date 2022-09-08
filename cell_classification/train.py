8b1c2ce5989084b6b791eae12c6c11743dc30e2e
Lenz <lorenz.rumberger@gmail.com>
2022-09-08 12:19:32 -0700

    Fixed an error in the parse_dict function that required tf running in eager mode

--- cell_classification/train.py
+++ cell_classification/train.py
@@ -7,23 +7,30 @@ from segmentation_data_prep import parse_dict, feature_description
 from data_generator import DataGenerator
 from segmentation_data_prep import parse_dict, feature_description
 
+tf.executing_eagerly()
 
 
 def train(params):
-    
+
     # make datasets and splits
-    dataset = tf.data.TFRecordDataset(params['record_path'])
+    dataset = tf.data.TFRecordDataset(params["record_path"])
     dataset = dataset.map(lambda x: tf.io.parse_single_example(x, feature_description))
     dataset = dataset.map(parse_dict)
-    validation_dataset = dataset.take(params['num_validation']) 
-    train_dataset = dataset.skip(params['num_validation'])
-    train_dataset = train_dataset.shuffle(params['shuffle_buffer_size'])
+    validation_dataset = dataset.take(params["num_validation"])
+    train_dataset = dataset.skip(params["num_validation"])
+    train_dataset = train_dataset.shuffle(params["shuffle_buffer_size"])
     augmenter = get_augmentation_pipeline(params)
-    train_generator = DataGenerator(train_dataset, params['raw_keys'], params['label_keys'], params['batch_size'],
-    augmentation_pipeline=augmenter)
+    train_generator = DataGenerator(
+        train_dataset,
+        params["raw_keys"],
+        params["label_keys"],
+        params["batch_size"],
+        augmentation_pipeline=augmenter,
+    )
     for batch in train_generator:
         print(batch.keys())
         break
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
