e37a90bc8405eab171aef0064472f077a101d90c
Lenz <lorenz.rumberger@gmail.com>
2022-09-13 13:50:33 -0700

    Added tests to prepare_tf_aug and py_aug

--- cell_classification/augmentation_pipeline_test.py
+++ cell_classification/augmentation_pipeline_test.py
@@ -1,7 +1,9 @@
 import pytest
 import numpy as np
-from augmentation_pipeline import augment_images, get_augmentation_pipeline
+from augmentation_pipeline import augment_images, get_augmentation_pipeline, prepare_tf_aug, py_aug
 import imgaug.augmenters as iaa
+import tensorflow as tf
+from copy import deepcopy
 
 parametrize = pytest.mark.parametrize
 
@@ -68,3 +70,62 @@ def test_augment_images(batch_num, chan_num):
     masks = np.zeros([batch_num, 100, 100], dtype=np.int32)
     _, augmented_masks = augment_images(images, masks, augmentation_pipeline)
     assert augmented_masks.shape == masks.shape
+
+
+def prepare_data(batch_num):
+    mplex_img = np.zeros([batch_num, 100, 100, 2], dtype=np.float32)
+    binary_mask = np.zeros([batch_num, 100, 100, 1], dtype=np.int32)
+    marker_activity_mask = np.zeros([batch_num, 100, 100, 1], dtype=np.int32)
+    mplex_img[0, :50, :50, :] = 10.1
+    mplex_img[0, 50:, 50:, :] = 201.12
+    binary_mask[0, :50, :50] = 1
+    binary_mask[0, 50:, 50:] = 1
+    marker_activity_mask[0, :50, :50] = 1
+    return mplex_img, binary_mask, marker_activity_mask
+
+
+@parametrize("batch_num", [1, 2, 3])
+def test_prepare_tf_aug(batch_num):
+    params = get_params()
+    augmentation_pipeline = get_augmentation_pipeline(params)
+    tf_aug = prepare_tf_aug(augmentation_pipeline)
+    mplex_img, binary_mask, marker_activity_mask = prepare_data(batch_num)
+    mplex_aug, mask_out, marker_activity_aug = tf_aug(
+        tf.constant(mplex_img, tf.float32), binary_mask, marker_activity_mask
+    )
+    mplex_img, binary_mask, marker_activity_mask = prepare_data(batch_num)
+    # check if right types and shapes are returned
+    assert type(mplex_aug) == np.ndarray
+    assert type(mask_out) == np.ndarray
+    assert type(marker_activity_aug) == np.ndarray
+    assert mplex_aug.dtype == np.float32
+    assert mask_out.dtype == np.int32
+    assert marker_activity_aug.dtype == np.int32
+    assert mplex_aug.shape == mplex_img.shape
+    assert mask_out.shape == binary_mask.shape
+    assert marker_activity_aug.shape == marker_activity_mask.shape
+
+
+@parametrize("batch_num", [1, 2, 3])
+def test_py_aug(batch_num):
+    params = get_params()
+    augmentation_pipeline = get_augmentation_pipeline(params)
+    tf_aug = prepare_tf_aug(augmentation_pipeline)
+    mplex_img, binary_mask, marker_activity_mask = prepare_data(batch_num)
+    batch = {
+        "mplex_img": tf.constant(mplex_img, tf.float32),
+        "binary_mask": tf.constant(binary_mask, tf.int32),
+        "marker_activity_mask": tf.constant(marker_activity_mask, tf.int32),
+        "dataset": "test_dataset",
+        "marker": "test_marker",
+        "imaging_platform": "test_platform",
+    }
+    batch_aug = py_aug(deepcopy(batch), tf_aug)
+
+    # check if right types and shapes are returned
+    for key in batch.keys():
+        assert isinstance(batch_aug[key], type(batch[key]))
+
+    for key in ["mplex_img", "binary_mask", "marker_activity_mask"]:
+        assert batch_aug[key].shape == batch[key].shape
+        assert not np.array_equal(batch_aug[key],  batch[key])
