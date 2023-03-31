7d05acc723cf7b879a8fb7318e66ea39fedd7154
Sricharan Reddy Varra <srivarra@stanford.edu>
2023-03-31 15:39:34 -0600

    fix import in tests

--- tests/simple_data_prep_test.py
+++ tests/simple_data_prep_test.py
@@ -3,7 +3,7 @@ import tempfile
 import os
 import numpy as np
 import pandas as pd
-from simple_data_prep import SimpleTFRecords
+from cell_classification.simple_data_prep import SimpleTFRecords
 from segmentation_data_prep_test import prepare_test_data_folders, prepare_cell_type_table
 import json
 import tempfile
