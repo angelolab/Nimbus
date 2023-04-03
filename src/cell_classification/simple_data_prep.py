56a5271bff1c207b6e69342242e91bb072ae62ca
Sricharan Reddy Varra <srivarra@stanford.edu>
2023-04-03 13:19:11 -0600

    simple_data_prep import fix

--- src/cell_classification/simple_data_prep.py
+++ src/cell_classification/simple_data_prep.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from .segmentation_data_prep import SegmentationTFRecords
+from segmentation_data_prep import SegmentationTFRecords
 
 
 class SimpleTFRecords(SegmentationTFRecords):
