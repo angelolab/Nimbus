44f6e477b310914aeadf0835f81ea98ef58f7a95
Sricharan Reddy Varra <srivarra@stanford.edu>
2023-04-17 11:21:40 -0700

    trying out casting tf to np 4

--- src/cell_classification/promix_naive.py
+++ src/cell_classification/promix_naive.py
@@ -344,11 +344,11 @@ class PromixNaive(ModelBuilder):
         selected_subset = []
         if positive_df.shape[0] > 0:
             selected_subset.append(
-                positive_df[positive_df.loss.numpy() < self.confidence_loss_thresholds["positive"]]
+                positive_df[positive_df.loss.to_numpy() < self.confidence_loss_thresholds["positive"]]
             )
         if negative_df.shape[0] > 0:
             selected_subset.append(
-                negative_df[negative_df.loss.numpy() < self.confidence_loss_thresholds["negative"]]
+                negative_df[negative_df.loss.to_numpy() < self.confidence_loss_thresholds["negative"]]
             )
         return selected_subset
 
