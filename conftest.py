5f9dee603379bb70f54e02a147e2752ee63d3763
Lenz <lorenz.rumberger@gmail.com>
2023-04-28 10:34:52 +0200

    Added rng fixture

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
 from typing import Any, Dict, Iterator
-
+import numpy as np
 import pytest
 import toml
 import os
@@ -13,3 +13,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "tests"))
 def config_params() -> Iterator[Dict]:
     params: Dict[str, Any] = toml.load("./configs/params.toml")
     yield params
+
+@pytest.fixture(scope="function")
+def rng() -> Iterator[np.random.Generator]:
+    rng_ = np.random.default_rng(seed=42)
+    yield rng_
