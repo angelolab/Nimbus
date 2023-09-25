7bb735e5be902aa18a244323ac526f093792cb93
Lenz <lorenz.rumberger@gmail.com>
2023-09-25 11:05:13 +0200

    Removed semantic_head.py from src/cell_classification

--- src/cell_classification/model_builder.py
+++ src/cell_classification/model_builder.py
@@ -12,6 +12,7 @@ from deepcell.panopticnet import PanopticNet
 from deepcell.utils import count_gpus
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.optimizers.schedules import CosineDecay
+from deepcell.semantic_head import create_semantic_head
 from tqdm import tqdm
 
 from cell_classification.augmentation_pipeline import (
@@ -21,7 +22,6 @@ from cell_classification.post_processing import (merge_activity_df,
                                                  process_to_cells)
 from cell_classification.segmentation_data_prep import (feature_description,
                                                         parse_dict)
-from cell_classification.semantic_head import create_semantic_head
 
 
 class ModelBuilder:
